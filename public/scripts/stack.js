ae2b2ab54bbcc0f355e9f34d640a43827942070c
Jack Tsang <cic_tian2@yahoo.com>
2019-10-23 17:48:08 -0700

    Added stack.js JavaScript implementation of Stack

---
+++ public/scripts/stack.js
@@ -0,0 +1,29 @@
+// Creates a stack
+var Stack = function() {
+    this.count = 0;
+    this.storage = {};
+}
+
+// Adds a value onto the end of the stack
+Stack.prototype.push = function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+}
+
+// Removes and returns the value at the end of the stack
+Stack.prototype.pop = function() {
+    // Check to see if the stack is empty
+    if (this.count === 0) {
+        return undefined;
+    }
+
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+}
+
+// Returns the length of the stack
+Stack.prototype.size = function() {
+    return this.count;
+}
