a3bf13f3a023396c92a41c8073e7047e76f137cd
Jack Tsang <cic_tian2@yahoo.com>
2019-10-27 22:48:33 -0700

    Added support for updating eventTag metadata

--- public/scripts/pgnflatten.js
+++ public/scripts/pgnflatten.js
@@ -40,6 +40,8 @@ moveData = pgn.split(" ");
 
 let moveCounter = 0;
 let legalMove = new RegExp(/^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$/);
+
+// Go through every single move in the PGN
 for (const move of moveData) {
     if (move.match(legalMove)) {
         mainMoveStack.push(move);
@@ -59,8 +61,9 @@ for (const move of moveData) {
         // Obtain the items from the move stack and put them into an array
         let generateRootMoves = mainMoveStack.printStack().trim().split(" ");
 
+        subVariationString = populateSubvariationMoveList(moveCounter, subVariationPly, generateRootMoves, moveNumberStack.peek());
         gameString = populateMoveList(gameString, generateRootMoves);
-        gameString = updatePGNMarkup(gameString,gameData); // Inserting and finalizing into Event Metadata
+        gameString = updatePGNMarkup(gameString, gameData, subVariationString); // Inserting and finalizing into Event Metadata
 
         // Add PGN to the output array
         pgnArray.push(gameString);
@@ -77,10 +80,14 @@ for (const move of moveData) {
     // End of main game, Append final Main PGN
     else if (move === "*" || move === "1/2-1/2" || move === "1-0" || move === "0-1") {
         let gameString = "";
+        let subVariationString = "";
+
+        // Obtain the items from the move stack and put them into an array
         let generateRootMoves = mainMoveStack.printStack().trim().split(" ");
 
+        subVariationString = populateSubvariationMoveList(moveCounter, moveCounter, generateRootMoves, 0); // 0 is white to move.
         gameString = populateMoveList(gameString, generateRootMoves);
-        gameString = updatePGNMarkup(gameString,gameData); // Inserting and finalizing into Event Metadata
+        gameString = updatePGNMarkup(gameString, gameData, subVariationString); // Inserting and finalizing into Event Metadata
 
         // Finalize the PGN
         pgnArray.push(gameString);
@@ -199,8 +206,13 @@ function removeExtraCharacters(game) {
 }
 
 /* Updates PGN with proper markup for future parsing */
-function updatePGNMarkup(gameString, gameData) {
-    let editedEventData = gameData.event[0].slice(0, -2) + ": " + gameString + gameData.event[0].slice(-2);
+function updatePGNMarkup(gameString, gameData, subVariationString) {
+    let editedEventData = "";
+    if (subVariationString === "") {
+        editedEventData = gameData.event[0].slice(0, -2) + ": " + gameString + gameData.event[0].slice(-2);
+    } else {
+        editedEventData = gameData.event[0].slice(0, -2) + ": " + subVariationString + gameData.event[0].slice(-2);
+    }
     gameString = gameData.date + '\n\n' + gameString;
     gameString = gameData.black + '\n' + gameString;
     gameString = gameData.white + '\n' + gameString;
@@ -218,6 +230,49 @@ function populateMoveList(gameString, moveArray) {
         // Blacks Turn
         else
             gameString += moveArray[i] + " ";
+    }
+    return gameString;
+}
+
+/*
+ * Current size of entire variation, sub-variation length, and move list is used to return only game notation
+ * of the sub variation eg:
+ *
+ * 1.e4 d6 ( 1... e5 ) will return 1... e5 as the move string.
+ */
+function populateSubvariationMoveList(currentMoveSize, lengthOfSubvariation, moveArray, sideToMove) {
+    let whiteToMove = false;
+    let blackToMove = false;
+
+    // If moveCounter is odd, then it is black's turn to move
+    if (sideToMove % 2 == 1) {
+        blackToMove = true;
+    }
+    // Otherwise it's whites turn
+    else if (sideToMove % 2 == 0) {
+        whiteToMove = true;
+    }
+    let gameString = "";
+    if (whiteToMove) {
+        for (let i = 0; i < lengthOfSubvariation; i++) {
+            // Calculation is divided by 2 + 1 since index starts at 0 and full moves are two plies
+            if (i % 2 === 0) {
+                gameString = (Math.floor((currentMoveSize - i - 1) / 2) + 1) + ". " + moveArray[moveArray.length - i - 1] + " " + gameString;
+            }
+            // Blacks Turn
+            else
+                gameString = moveArray[moveArray.length - i - 1] + " " + gameString;
+        }
+    } else if (blackToMove) {
+        for (let i = 0; i < lengthOfSubvariation; i++) {
+            // Calculation is divided by 2 + 1 since index starts at 0 and full moves are two plies
+            if (i % 2 === 1) {
+                gameString = (Math.floor((currentMoveSize - i - 1) / 2)) + ". " + moveArray[moveArray.length - i - 1] + " " + gameString;
+            }
+            // Blacks Turn
+            else if (i === lengthOfSubvariation - 1)
+                gameString = (Math.floor((currentMoveSize - i - 1) / 2)) + "... " + moveArray[moveArray.length - i - 1];
+        }
     }
     return gameString;
 }
